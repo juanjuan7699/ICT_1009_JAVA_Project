f1b5c398aeb3ab866da1971a9880b571b7a737ca
DoctorDoge <aloysiuslee97@hotmail.com>
2021-02-27 17:02:18 +0800

    Removed unused variables

--- core/src/com/ict1009/ahg/AnimalHunter.java
+++ core/src/com/ict1009/ahg/AnimalHunter.java
@@ -3,22 +3,14 @@ package com.ict1009.ahg;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.ict1009.ahg.screens.GameScreen;
-import com.ict1009.ahg.screens.MenuScreen;
 import com.ict1009.ahg.screens.SplashScreen;
 
 public class AnimalHunter extends Game {
-	private GameScreen gameScreen;
-	private MenuScreen menuScreen;
-	private SplashScreen splashScreen;
 	static public Skin gameSkin;
 
 	@Override
 	public void create () {
-//		gameScreen = new GameScreen(this);
-//		menuScreen = new MenuScreen(this);
 		gameSkin = new Skin(Gdx.files.internal("glassy-ui.json"));
-//		this.setScreen(new MenuScreen(this));
 		this.setScreen(new SplashScreen(this));
 	}
 
